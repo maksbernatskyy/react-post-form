aee92b6bfd5a69df74b87a7578cabbdaa85cb717
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-13 12:42:55 +0100

    add inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,35 @@ export default function Main() {
                     <div className="card-body">
                         {/* Form */}
                         <form>
-                            
+                            <div className="d-flex flex-column gap-3">
+                                {/* Author */}
+                                <input 
+                                    className="w-25" 
+                                    type="text"
+                                    placeholder="Author" 
+                                />
+                                {/* Title */}
+                                <input 
+                                    className="w-25" 
+                                    type="text" 
+                                    placeholder="Title"
+                                />
+                                {/* Body */}
+                                <input 
+                                    className="w-25" 
+                                    type="text"
+                                    placeholder="Body" 
+                                />
+                                {/* Public */}
+                                <input 
+                                    className="align-self-start" 
+                                    type="checkbox" 
+                                />
+                                <input
+                                    className="align-self-start" 
+                                    type="checkbox" 
+                                />
+                            </div>
                         </form>
                     </div>
                 </div>
