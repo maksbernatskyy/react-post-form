b7dc5915a80647d6f035b11afbbd962e9f73252a
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-13 12:26:44 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ export default function Header() {
 
     return (
         <>
-        
+        <header className="p-3">
+            <h1 className="text-uppercase">post form</h1>
+        </header>
         </>
     )
 }
