55af50fe4ce1cae219e468353acc1dbaf775c259
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-13 12:31:39 +0100

    add card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,26 @@ export default function Main() {
 
     return (
         <>
-        
+        <main>
+            <div className="container">
+
+                {/* Card */}
+                <div className="card p-3">
+                    {/* Card Top */}
+                    <div className="card-top">
+                        <h2 className="text-uppercase text-primary">form</h2>
+                    </div>
+
+                    {/* Card Bottom */}
+                    <div className="card-body">
+                        {/* Form */}
+                        <form>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
         </>
     )
 }
